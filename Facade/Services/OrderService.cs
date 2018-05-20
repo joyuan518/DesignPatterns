54d7e27731f0f0fae7af7bb67325fbc7fa5f21e1
John Yuan <john.yuan@bleum.com>
2018-05-20 23:34:31 +0800

    Complete "Facade" pattern.

--- Facade/Services/OrderService.cs
+++ Facade/Services/OrderService.cs
@@ -1,7 +1,12 @@
 namespace DesignPatterns.Facade.Services
 {
+    using Entities;
+
     public class OrderService
     {
-        
+        public Order GetOrder(int orderId)
+        {
+            return new Order();
+        }
     }
 }
