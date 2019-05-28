b9838432de441c172a6e1af177f60b7552243f99
John Yuan <john.yuan@bleum.com>
2019-05-28 19:47:26 +0800

    Update Client.cs

--- Facade/Client.cs
+++ Facade/Client.cs
@@ -10,7 +10,7 @@ namespace DesignPatterns.Facade
             //the specific consumer of the system services will consume them via the gateway services intead of 
             // invoking them directly. the consumer doesn't need to be aware of the underlying services, 
             // which might be designed to be in a good shape as public services for many consumers, but 
-            // might not be straightfoward endough for one specific consumer. the gateway/facade services 
+            // might not be straightfoward enough for one specific consumer. the gateway/facade services 
             // provide easy interface for specific consumers, and also decouple them from the detail of the 
             // underlynig services impelmentation
             var orderId = 1;
